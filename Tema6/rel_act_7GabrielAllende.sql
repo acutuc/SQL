4fc245e4067a461771bc4e6b632c118d0ad3f257
acutuc <acutuc94@gmail.com>
2022-03-24 10:25:54 +0100

    Actualizados ejercicios

--- Tema6/rel_act_7GabrielAllende.sql
+++ Tema6/rel_act_7GabrielAllende.sql
@@ -7,12 +7,12 @@ HAVING count(empleados.numem) > 1;
 
 /*2. Para cada extensión telefónica, hallar cuantos empleados la usan y el salario medio de éstos.
 Solo nos interesan aquellos grupos en los que hay entre 1 y 3 empleados.*/
-SELECT empleados.extelem as 'ExtensiónTelefónica', count(empleados.numem) as 'CantidadEmpleados', avg(empleados.salarem) as 'SalarioMedio'
+SELECT empleados.extelem as 'ExtensiónTelefónica', count(empleados.numem) as 'CantidadEmpleados', empleados.salarem as 'SalarioMedio'
 FROM empleados
 GROUP BY empleados.extelem
 HAVING count(empleados.numem) between 1 and 3;
 
--- use ventapromoscompleta
+use ventapromoscompleta
 /*3. Prepara un procedimiento que, dada un código de promoción obtenga un listado con el nombre de las categorías
 que tienen al menos dos productos incluidos en dicha promoción.*/
 drop procedure if exists ej3_7;
@@ -42,27 +42,21 @@ drop procedure if exists ej5_7;
 delimiter $$
 create procedure ej5_7(in anio year)
 BEGIN
-	SELECT sum(detalleventa.cant * detalleventa.precioventa) as 'Importe Total Ventas', monthname(fecventa) as 'Mes'
+	SELECT sum(detalleventa.cant * detalleventa.precioventa) as 'Importe Total Ventas'
     FROM detalleventa JOIN ventas ON detalleventa.codventa = ventas.codventa
-    WHERE anio = year(ventas.fecventa)
-    GROUP BY monthname(ventas.fecventa);
+    WHERE anio = ventas.fecventa
+    GROUP BY month(ventas.fecventa);
 END $$
 delimiter ;
-call ej5_7(2012);
+call ej5_7(2020);
 
 /*6. Como el ejercicio anterior, pero ahora solo nos interesa mostrar aquellos meses en los que se ha superado a la media del año.*/
 drop procedure if exists ej6_7;
 delimiter $$
-create procedure ej6_7(in anio year)
+create procedure ej6_7()
 BEGIN
-	SELECT sum(detalleventa.cant * detalleventa.precioventa) as 'Importe Total Ventas', monthname(fecventa) as 'Mes'
-    FROM detalleventa JOIN ventas ON detalleventa.codventa = ventas.codventa
-    WHERE anio = year(ventas.fecventa)
-    GROUP BY monthname(ventas.fecventa)
-    HAVING sum(detalleventa.precioventa * detalleventa.cant) > (SELECT avg(detalleventa.precioventa * detalleventa.cant)
-																FROM ventas JOIN detalleventa
-																	ON ventas.codventa = detalleventa.codventa
-																WHERE year(fecventa) = anio);
+	SELECT
+    FROM 
+    
 END $$
 delimiter ;
-call ej6_7(2012);
