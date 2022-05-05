c3c688afdb74d802793df5ccae69875febb1be78
acutuc <acutuc94@gmail.com>
2022-05-05 10:26:15 +0200

    Subido examen Tema 7

---
+++ Tema7/ExamenU7GabrielAllende.sql
@@ -0,0 +1,106 @@
+-- Exámen T7 Gabriel Allende.
+
+use GBDgestionaTests;
+
+delimiter $$
+drop procedure if exists examenU7 $$
+create procedure examenU7 (in curso char(6))
+begin
+
+	declare cursoMateria char(6) DEFAULT '0';
+	declare nombreMateria, nombreMateriaAux, descripTest varchar(60) DEFAULT '';
+    declare cantPreguntasTest, diasDesdePublicacion int DEFAULT 0;
+    declare respuestaValida char(1) DEFAULT '0';
+    declare fechaPublicacion datetime DEFAULT '1000-01-01';
+    declare mediaPreguntas decimal(4,2) DEFAULT 0;
+    declare mediaDiasPublicacion decimal(4,2) DEFAULT 0;
+    
+    declare primeraIteracion boolean default true;
+    declare finalcursor boolean default false;
+    
+    declare cursorMaterias cursor for
+						SELECT materias.cursomateria, materias.nommateria, tests.descrip, count(preguntas.resvalida), datediff(curdate(), tests.fecpublic)
+						FROM materias JOIN tests on materias.codmateria = tests.codmateria JOIN preguntas on tests.codtest = preguntas.codtest
+						GROUP BY materias.cursomateria, materias.nommateria, tests.descrip, datediff(curdate(), tests.fecpublic);
+	
+    DECLARE CONTINUE HANDLER FOR SQLSTATE '02000' 
+				SET finalcursor = true;
+    
+    drop table if exists listado;
+	create temporary table listado
+		(descripcion varchar(200));
+    
+	open cursorMaterias;
+    FETCH NEXT FROM cursorMaterias INTO cursoMateria, nombreMateria, descripTest, respuestaValida, fechaPublicacion;
+    
+    INSERT INTO listado 
+        VALUES 
+            (concat('MATERIAS DEL CURSO'), curso),
+            ('--------------------------------------------------------------------------------------------------------------');
+        
+    while not finalcursor do
+    begin
+		if nombreMateria <> nombreMateriaAux then
+        begin
+			if primeraIteracion = false then
+			begin
+                set mediaPreguntas = count(preguntas.resvalida)/preguntas.resvalida;
+                set mediaDiasPublicacion = fechaPublicacion/datediff(curdate(), tests.fecpublic);
+			end;
+			else
+					set mediaPreguntas = 0;
+                    set mediaDiasPublicacion = 0;
+			end if;
+			end;
+				INSERT INTO listado 
+					-- (descripcion) 
+				VALUES
+					('--------------------------------------------------------------------------------------------------------------'),
+					(concat_ws(' ', 'Media de ', nombreMateriaAux, ':       ',mediaPreguntas), '            ', mediaDiasPublicacion),
+					('--------------------------------------------------------------------------------------------------------------');
+				set primeraIteracion = false;
+                set mediaPreguntas = 0;
+                set fechaPublicacion = '0';
+                set nombreMateriaAux = nombreMateria;
+            else
+				set primeraIteracion = false;
+			end if;
+			INSERT INTO listado 
+            -- (descripcion) 
+			VALUES
+				(concat('MATERIA: ', nombreMateria)),
+                ('Test             Núm. preguntas test               días desde la publicación'),
+				('--------------------------------------------------------------------------------------------------------------');
+			set nombreMateriaAux = nombreMateria;
+            
+            
+			INSERT INTO listado
+			VALUES
+				(concat(descripTest, '                 ', mediaPreguntas, '                     ', mediaDiasPublicacion));
+                
+                set descripTest = '';
+                set mediaPreguntas = 0;
+                set mediaDiasPublicacion = 0;
+                FETCH NEXT FROM cursorMaterias INTO cursoMateria, nombreMateria, descripTest, respuestaValida, fechaPublicacion;
+        end;
+	end while;
+	close cursorMaterias;
+        set mediaPreguntas = count(preguntas.resvalida)/preguntas.resvalida;
+		set mediaDiasPublicacion = fechaPublicacion/datediff(curdate(), tests.fecpublic);
+		INSERT INTO listado 
+		-- (descripcion) 
+		VALUES
+			('--------------------------------------------------------------------------------------------------------------'),
+			(concat_ws(' ', 'Media de ', nomClienteAux, ' en ', mesVentaAux, ':       ',	
+					sumCantVentas/numVentas), puntosRegalados),
+			('--------------------------------------------------------------------------------------------------------------');
+		SELECT *
+        FROM listado
+	end;
+		SELECT CONCAT('No existen cursos en ', curso);
+        
+    drop table if exists listado;
+end $$
+delimiter ;
+
+CALL examenU7 (1);
--- Tema7/simulacionExamen.sql
+++ Tema7/simulacionExamen.sql
@@ -58,123 +58,192 @@ Media de Nombre de cliente en MARZO:	Media unidades compradas*		puntos regalados
 ……..
 */
 
-use ventapromoscompleta;
-
-drop procedure if exists SIMULACRO_EXAMEN_U7;
-DELIMITER $$
-CREATE PROCEDURE SIMULACRO_EXAMEN_U7()
-BEGIN
-
--- CALL SIMULACRO_EXAMEN_U7;
-
-DECLARE nombrecasa, nomcasaaux varchar(100) default '';
-DECLARE nombrezona, nomzonaaux varchar(100) default '';
-DECLARE nombrecaracteristica varchar(100) default '';
-DECLARE metros, baños, habitaciones int default 0;
-DECLARE preciocasa, preciototalzona decimal(9,2) default 0;
-DECLARE primeraZona, primeraCasa boolean default 1;
-DECLARE numcasaszona int default 0;
-
-
-DECLARE DesCaracteristicas varchar(4000) default '';
-
-
-DECLARE final bit default 0;
-
-DECLARE curCasas CURSOR
-	FOR SELECT nomcasa, m2, numbanios, numhabit, preciobase, nomzona, nomcaracter
-	-- (select avg(preciobase) from casas as c where casas.codzona = c.codzona) as mediazona /* no es eficiente ya que se calcula la media de la zona de cada casa*/
-		FROM casas join zonas on casas.codzona = zonas.numzona
-			join caracteristicasdecasas on casas.codcasa = caracteristicasdecasas.codcasa
-				join caracteristicas on caracteristicasdecasas.codcaracter = caracteristicas.numcaracter
-		ORDER BY nomzona, nomcasa;
-
-
-DECLARE CONTINUE HANDLER FOR SQLSTATE '02000' SET final = 1;
-
-drop table if exists listado;
-create temporary table listado
-    (descripcion varchar(4000));
-
-OPEN curCasas;
-FETCH FROM curCasas INTO nombrecasa, metros, baños, habitaciones, preciocasa, nombrezona, nombrecaracteristica;
--- WHILE not final DO
-WHILE final = 0 DO
-BEGIN
+delimiter $$
+drop procedure if exists ExamU7G1 $$
+create procedure ExamU7G1 (
+	in mesini int,
+    in mesfin int)
+begin
+	-- call ExamU7G1(1,6);
+	declare nomCliente, nomClienteAux, nomClienteAnterior varchar(100) default '';
+    declare codigoCliente, codigoClienteAux int default 0;
+    declare mesVenta, mesVentaAux varchar(12) default '';
+    declare nombreArticulo varchar(100) default '';
+    declare numVentas, cantVenta, sumCantVentas int default 0;
+    declare precVenta, sumPrecVentas decimal(7,2) default 0;
+    declare mediaVentaEmpresa decimal(7,2) default 0;
+    declare puntosRegalados int default 0;
+    
+    declare primeraIteracion boolean default true;
+    declare finalcursor boolean default false;
+    
+    declare curClientes cursor for
+						select clientes.codcli, concat_ws(' ', clientes.nomcli, clientes.ape1cli, clientes.ape2cli),
+								monthname(ventas.fecventa), articulos.nomart, detalleventa.cant, detalleventa.precioventa
+						from ventas join clientes on ventas.codcli=clientes.codcli
+							join detalleventa on ventas.codventa = detalleventa.codventa
+								join articulos on detalleventa.refart = articulos.refart
+						where month(ventas.fecventa)  between 1 and 6 --  mesini and mesfin
+-- 							and year(ventas.fecventa) = year(curdate())
+						order by concat_ws(' ', clientes.nomcli, clientes.ape1cli, clientes.ape2cli), 
+							month(ventas.fecventa);
 	
-    IF nomzonaaux <> nombrezona THEN
-		begin
-		
-			set nomzonaaux = nombrezona;
-			IF (primeraZona = 1) THEN
-				set primeraZona = 0;
-			ELSE
-				BEGIN
-					INSERT INTO listado 
-						(descripcion) 
-					VALUES
-						(concat('PRECIO MEDIO DE LA ZONA: ', preciototalzona/numcasaszona));
-					set numcasaszona = 0;
-				END;
-			END IF;
+    declare continue handler FOR SQLSTATE '02000' 
+				SET finalcursor = true;
+/*	declare exit handler FOR sqlexception
+				begin
+					rollback;
+                    close curClientes;
+                end;
+*/ -- 	start transaction;
     
-			INSERT INTO listado 
-				(descripcion) 
-			VALUES 
-				(concat('ZONA: ', nombrezona));
-			set primeraCasa = 1;
-			
-			set preciototalzona = 0;
-		end;
-	END IF;
+    drop table if exists listado;
+	create temporary table listado
+		(descripcion varchar(200));
     
+	open curClientes;
+    fetch next from curClientes into codigoCliente, nomCliente, mesVenta, nombreArticulo, cantVenta, precVenta;
     
-    if nomcasaaux <> nombrecasa then
-	begin
-		IF (primeraCasa = 1) THEN
-			set primeraCasa = 0;
-		ELSE 
-			begin
-				-- guardarmos los datos del anterior grupo
-				INSERT INTO listado 
-					(descripcion) 
-				VALUES
-					('CARACTERÍSTICAS:'),
-					(DesCaracteristicas);
-				set DesCaracteristicas= '';
-			end;
-		END IF;
-    
-		INSERT INTO listado 
-            (descripcion) 
+    INSERT INTO listado 
+            -- (descripcion) 
         VALUES 
-            (concat('CASA: ', nombrecasa,' (METROS:', metros, ', BAÑOS: ', baños, ', HABIT: ',habitaciones, ')'));
-		set preciototalzona = preciototalzona + preciocasa;
-        set numcasaszona = numcasaszona + 1;
-        set nomcasaaux = nombrecasa;
+            (CONCAT('COMPRAS DE CLIENTES DE: ',  
+					upper(monthname(convert(concat('2020/',mesini,'/1'),date))), ' A ', 
+                     upper(monthname(convert(concat('2020/',mesfin,'/1'),date)))
+					)
+			),
+            ('--------------------------------------------------------------------------------------------------------------');
+        
+    while not finalcursor do
+    begin
+		if nomCliente <> nomClienteAux then
+        begin
+			if primeraIteracion = false then
+			begin
+				if sumCantVentas/numVentas >= mediaVentaEmpresa then
+                begin
+					set puntosRegalados = 5;
+                    update clientes
+					set ptosacumulados = ptosacumulados + 5
+                    where codcli = codigoClienteAux;
+                    
+				end;
+                else
+					set puntosRegalados = 0;
+                end if;
+				INSERT INTO listado 
+					-- (descripcion) 
+				VALUES
+					('--------------------------------------------------------------------------------------------------------------'),
+					(concat_ws(' ', 'Media de ', nomClienteAux, ' en ', mesVentaAux, ':       ',	
+							sumCantVentas/numVentas),'------',   puntosRegalados),
+					('--------------------------------------------------------------------------------------------------------------');
+				set primeraIteracion = false;
+                set numVentas = 0;
+                set sumCantVentas = 0;
+                set SumPrecVentas = 0;
+                set codigoClienteAux = codigoCliente;
+			end;
+            else
+				set primeraIteracion = false;
+			end if;
+			INSERT INTO listado 
+            -- (descripcion) 
+			VALUES
+				(concat('CLIENTE: ', nomCliente)),
+				('--------------------------------------------------------------------------------------------------------------');
+			set nomClienteAux = nomCliente;
+            set mesVentaAux = ''; -- inicializo la variable
+        end;
+        end if;
+    
+		if mesVenta <> mesVentaAux then
+        
+        begin
+			if not primeraIteracion and mesVentaAux <> '' then
+			begin
+				if sumCantVentas/numVentas >= mediaVentaEmpresa then
+                begin
+					set puntosRegalados = 5;
+                    update clientes
+					set ptosacumulados = ptosacumulados + 5
+                    where codcli =  codigoClienteAux;
+                    
+				end;
+                else
+					set puntosRegalados = 0;
+                end if;
+				INSERT INTO listado 
+					-- (descripcion) 
+				VALUES
+					('--------------------------------------------------------------------------------------------------------------'),
+					(concat_ws(' ', 'Media de ', nomClienteAux, ' en ', mesVentaAux, ':       ',	
+							sumCantVentas/numVentas),'------', puntosRegalados),
+					('--------------------------------------------------------------------------------------------------------------');
+				set primeraIteracion = false;
+                set numVentas = 0;
+                set sumCantVentas = 0;
+                set SumPrecVentas = 0;
+                set codigoClienteAux = codigoCliente;
+			end;
+			end if;
+            
+			INSERT INTO listado 
+            -- (descripcion) 
+			VALUES
+				(upper(mesVenta)),
+                ('ArtÃƒÂ­culo               			-	            	Unidades compradas                     			Precio de compra');
+
+			set mesVentaAux = mesVenta;
+            -- calculo la media de unidades vendidas por venta en la empresa en ese mes:
+			set mediaVentaEmpresa = (select sum(cant)/count(*) 
+							from detalleventa join ventas on detalleventa.codventa = ventas.codventa
+							where month(ventas.fecventa) = mesVenta
+-- 								and year(ventas.fecventa) = year(curdate()
+							);
+		end;
+        end if;
+        
+        INSERT INTO listado 
+            -- (descripcion) 
+		VALUES
+				(concat(nombreArticulo, '               ',cantVenta,'                   ', precVenta));
+
+		set numVentas = numVentas+1;
+        set sumCantVentas = sumCantVentas + cantVenta;
+        
+		fetch next from curClientes into codigoCliente, nomCliente, mesVenta, nombreArticulo, cantVenta, precVenta;
     end;
-    end if;
-    set DesCaracteristicas = concat(DesCaracteristicas, ', ', nombrecaracteristica);
-
-	FETCH FROM curCasas INTO nombrecasa, metros, baños, habitaciones, preciocasa, nombrezona, nombrecaracteristica;
-END;
-END WHILE;
-CLOSE curCasas;
-
-if (select count(*) from listado) > 0 then
-BEGIN
-	INSERT INTO listado 
-		(descripcion) 
-	VALUES
-		('CARACTERÍSTICAS:'),
-		(DesCaracteristicas),
-        (concat('PRECIO MEDIO DE LA ZONA: ', preciototalzona/numcasaszona));
-	
-    select * from listado;
-END;
-else
-    select 'NO EXISTEN CASAS';
-end if;
-drop table if exists listado;
-END $$
-DELIMITER ;
+    end while;
+    
+    close curClientes;
+    if (select count(*) from listado) > 0 then
+    begin
+		if sumCantVentas/numVentas >= mediaVentaEmpresa then
+		begin
+			set puntosRegalados = 5;
+			update clientes
+			set ptosacumulados = ptosacumulados + 5
+			where codcli = codigoClienteAux;
+		end;
+        end if;
+		INSERT INTO listado 
+		-- (descripcion) 
+		VALUES
+			('--------------------------------------------------------------------------------------------------------------'),
+			(concat_ws(' ', 'Media de ', nomClienteAux, ' en ', mesVentaAux, ':       ',	
+					sumCantVentas/numVentas), puntosRegalados),
+			('--------------------------------------------------------------------------------------------------------------');
+		select * from listado;
+	end;
+    else
+		select CONCAT('No existen ventas entre los meses ',
+					   monthname(convert(concat('2020/',mesini,'/1'),date)), ' y ', 
+					   monthname(convert(concat('2020/',mesfin,'/1'),date))
+					 );
+	end if;
+	drop table if exists listado;
+--     commit;
+    
+end $$
+delimiter ;
